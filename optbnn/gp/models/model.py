8dadda36a088a82d16ef2f9f2cc83aa89c49e86a
Loren Champlin <champlin@email.arizona.edu>
2025-06-23 16:39:13 -0700

    added lcfModel

--- optbnn/gp/models/model.py
+++ optbnn/gp/models/model.py
@@ -46,8 +46,9 @@ class GPModel(torch.nn.Module):
     >>> m.Y = Ynew
     """
 
-    def __init__(self, X, Y, kern, likelihood, mean_function,
-                 name=None, jitter_level=1e-6):
+    def __init__(
+        self, X, Y, kern, likelihood, mean_function, name=None, jitter_level=1e-6
+    ):
         super(GPModel, self).__init__()
         self.name = name
         self.mean_function = mean_function or mean_functions.Zero()
@@ -57,11 +58,11 @@ class GPModel(torch.nn.Module):
 
         if isinstance(X, np.ndarray):
             # X is a data matrix; each row represents one instance
-            X = torch.from_np(X)
+            X = torch.from_numpy(X)
         if isinstance(Y, np.ndarray):
             # Y is a data matrix, rows correspond to the rows in X,
             # columns are treated independently
-            Y = torch.from_np(Y)
+            Y = torch.from_numpy(Y)
         self.X, self.Y = X, Y
 
     @abc.abstractmethod
@@ -113,7 +114,10 @@ class GPModel(torch.nn.Module):
                 prior_params.append(param)
         if len(prior_params) == 0:
             var = self.kern.K(X)
-            jitter = torch.eye(mu.size(0), dtype=mu.dtype, device=mu.device) * self.jitter_level
+            jitter = (
+                torch.eye(mu.size(0), dtype=mu.dtype, device=mu.device)
+                * self.jitter_level
+            )
             samples = []
             for i in range(self.num_latent):
                 L = torch.cholesky(var + jitter, upper=False)
@@ -127,13 +131,16 @@ class GPModel(torch.nn.Module):
                     with torch.no_grad():
                         param.copy_(param.prior.sample())
                 var = self.kern.K(X)
-                jitter = torch.eye(mu.size(0), dtype=mu.dtype, device=mu.device) * self.jitter_level
+                jitter = (
+                    torch.eye(mu.size(0), dtype=mu.dtype, device=mu.device)
+                    * self.jitter_level
+                )
                 s = []
                 for i in range(self.num_latent):
                     multiplier = 1
                     while True:
                         try:
-                            L = torch.cholesky(var + multiplier*jitter, upper=False)
+                            L = torch.cholesky(var + multiplier * jitter, upper=False)
                             break
                         except RuntimeError as err:
                             multiplier *= 2
@@ -141,7 +148,6 @@ class GPModel(torch.nn.Module):
                     s.append(mu + torch.matmul(L, V))
                 samples.append(torch.stack(s, dim=0).permute(1, 2, 0))
             return torch.cat(samples, dim=1)
-        
 
     def predict_f_samples(self, Xnew, num_samples):
         """
@@ -149,13 +155,15 @@ class GPModel(torch.nn.Module):
         Xnew.
         """
         mu, var = self.predict_f(Xnew, full_cov=True)
-        jitter = torch.eye(mu.size(0), dtype=mu.dtype, device=mu.device) * self.jitter_level
+        jitter = (
+            torch.eye(mu.size(0), dtype=mu.dtype, device=mu.device) * self.jitter_level
+        )
         samples = []
-        for i in range(self.num_latent): # TV-Todo: batch??
+        for i in range(self.num_latent):  # TV-Todo: batch??
             L = torch.cholesky(var[:, :, i] + jitter, upper=False)
             V = torch.randn(L.size(0), num_samples, dtype=L.dtype, device=L.device)
-            samples.append(mu[:, i:i + 1] + torch.matmul(L, V))
-        return torch.stack(samples, dim=0) # TV-Todo: transpose?
+            samples.append(mu[:, i : i + 1] + torch.matmul(L, V))
+        return torch.stack(samples, dim=0)  # TV-Todo: transpose?
 
     def predict_y(self, Xnew, full_cov=False):
         """
@@ -175,10 +183,77 @@ class GPModel(torch.nn.Module):
         return self.likelihood.predict_density(pred_f_mean, pred_f_var, Ynew)
 
     def _repr_html_(self):
-        s =  'Model {}<ul>'.format(type(self).__name__)
-        for n,c in self.named_children():
-            s += '<li>{}: {}</li>'.format(n, type(c).__name__)
-        s += '</ul><table><tr><th>Parameter</th><th>Value</th><th>Prior</th><th>ParamType</th></tr><tr><td>'
-        s += '</td></tr><tr><td>'.join(['</td><td>'.join((n,str(p.get().data.cpu().numpy()),str(p.prior),type(p).__name__)) for n,p in self.named_parameters()])
-        s += '</td></tr></table>'
+        s = "Model {}<ul>".format(type(self).__name__)
+        for n, c in self.named_children():
+            s += "<li>{}: {}</li>".format(n, type(c).__name__)
+        s += "</ul><table><tr><th>Parameter</th><th>Value</th><th>Prior</th><th>ParamType</th></tr><tr><td>"
+        s += "</td></tr><tr><td>".join(
+            [
+                "</td><td>".join(
+                    (n, str(p.get().data.cpu().numpy()), str(p.prior), type(p).__name__)
+                )
+                for n, p in self.named_parameters()
+            ]
+        )
+        s += "</td></tr></table>"
         return s
+
+
+class LCFModel(torch.nn.Module):
+    """
+    A class for Gaussian Process in the form of a linear combination
+    of functions (LCF)
+
+    They take two main initial arguments, a covariance matrix and a function that
+    takes inputs and evaluates them over a vector of functions
+
+    The covariance arg can take a (n_concepts, ) 1D array or a symmetric (n_concepts,n_concepts) 2D array
+
+    The function_vect arg takes a function that takes (n_samples,n_raw_features) 2D array
+    as input and outputs a (n_samples,n_concepts) 2D array
+
+    Its up to the function_vect arg to provide a row of ones for an intercept term
+
+    Optionally, a (n_concepts,) 1D array for the mean argument can be given
+
+    If mean is not given, a (n_concepts,) 1D array of zeros is automatically generated
+
+    This does not have working forward function (for now)
+    """
+
+    def __init__(self, covariance, function_vect, mean=None, name=None):
+        super(LCFModel, self).__init__()
+        self.name = name
+        self.function_vect = function_vect
+
+        if isinstance(covariance, np.ndarray):
+            if covariance.ndim == 1:
+                covariance = np.diag(covariance)
+            covariance = torch.from_numpy(covariance)
+        else:
+            if covariance.ndim == 1:
+                covariance = torch.diag(covariance)
+
+        if mean is not None:
+            if isinstance(mean, np.ndarray):
+                mean = torch.from_numpy(mean)
+        else:
+            mean = torch.zeros(covariance.size(0))
+        self.weight_generator = (
+            torch.distributions.multivariate_normal.MultivariateNormal(mean, covariance)
+        )
+
+    def forward(self, X=None, Y=None):
+        pass
+
+    def sample_functions(self, X, num_samples, aux_X=None):
+        """
+        Produce samples from the prior latent functions at the points X.
+        X must be a (n_samples, n_raw_features) 2D array
+        If given, its assumed aux_X.shape[0] == X.shape[0]
+        """
+        if aux_X is not None:
+            Y = self.function_vect(X, aux_X)
+        else:
+            Y = self.function_vect(X)
+        return torch.mm(Y, self.weight_generator.sample(torch.Size([num_samples])).T).unsqueeze(-1)
