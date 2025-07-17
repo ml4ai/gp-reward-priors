89caf558ac97ad40a08f08e1c317c2145fb3d73f
Loren Champlin <champlin@email.arizona.edu>
2025-07-17 10:39:58 -0700

    new reward functions

--- optbnn/gp/reward_functions.py
+++ optbnn/gp/reward_functions.py
@@ -13,33 +13,62 @@ import numpy as np
 # LCFmodel class works
 def pen_task_reward_prior(X, aux_X):
     if isinstance(aux_X, np.ndarray):
-        aux_X = torch.from_np(aux_X)
-        
-    intercept = torch.ones(aux_X.size(0)).float()
-    
-    goal_distance = torch.linalg.norm(aux_X[:,39:42],dim=1)
-    orien_similarity = torch.einsum('ij,ij->i',aux_X[:,33:36],aux_X[:,36:39])
-    
-    close = ((goal_distance < 0.075) & (orien_similarity > 0.9)).float()
-    
-    closer = ((goal_distance < 0.075) & (orien_similarity > 0.95)).float()
-    
-    dropped = (aux_X[:,26] < 0.075).float()
+        aux_X = torch.from_numpy(aux_X)
 
-    return torch.stack([intercept,goal_distance,orien_similarity,close,closer,dropped],dim=1)
+    intercept = torch.ones(aux_X.size(0)).double()
+
+    goal_distance = torch.linalg.norm(aux_X[:, 39:42], dim=1).double()
+    orien_similarity = torch.einsum(
+        "ij,ij->i", aux_X[:, 33:36], aux_X[:, 36:39]
+    ).double()
+
+    close = ((goal_distance < 0.075) & (orien_similarity > 0.9)).double()
+
+    closer = ((goal_distance < 0.075) & (orien_similarity > 0.95)).double()
+
+    dropped = (aux_X[:, 26] < 0.075).double()
+
+    return torch.stack(
+        [intercept, -goal_distance, orien_similarity, close, closer, -dropped], dim=1
+    ).double()
+
 
 # Same as above, no intercept
 def pen_task_reward_prior_no_intercept(X, aux_X):
     if isinstance(aux_X, np.ndarray):
-        aux_X = torch.from_np(aux_X)
-        
-    goal_distance = torch.linalg.norm(aux_X[:,39:42],dim=1)
-    orien_similarity = torch.einsum('ij,ij->i',aux_X[:,33:36],aux_X[:,36:39])
-    
-    close = ((goal_distance < 0.075) & (orien_similarity > 0.9)).float()
-    
-    closer = ((goal_distance < 0.075) & (orien_similarity > 0.95)).float()
-    
-    dropped = (aux_X[:,26] < 0.075).float()
+        aux_X = torch.from_numpy(aux_X)
 
-    return torch.stack([goal_distance,orien_similarity,close,closer,dropped],dim=1)
+    goal_distance = torch.linalg.norm(aux_X[:, 39:42], dim=1).double()
+    orien_similarity = torch.einsum(
+        "ij,ij->i", aux_X[:, 33:36], aux_X[:, 36:39]
+    ).double()
+
+    close = ((goal_distance < 0.075) & (orien_similarity > 0.9)).double()
+
+    closer = ((goal_distance < 0.075) & (orien_similarity > 0.95)).double()
+
+    dropped = (aux_X[:, 26] < 0.075).double()
+
+    return torch.stack(
+        [-goal_distance, orien_similarity, close, closer, -dropped], dim=1
+    ).double()
+
+
+# BB reward function. Again, just aux_X is used here. Assumes the state contains 6 closest obstacles
+def bb_reward_prior(X, aux_X):
+    if isinstance(aux_X, np.ndarray):
+        aux_X = torch.from_numpy(aux_X)
+
+    intercept = torch.ones(aux_X.size(0)).double()
+
+    goal_distance = torch.sqrt(
+        (aux_X[:, 20] - aux_X[:, 0]) ** 2 + (aux_X[:, 21] - aux_X[:, 1]) ** 2
+    ).double()
+
+    min_obs_dist = torch.sqrt(
+        (aux_X[:, 2] - aux_X[:, 0]) ** 2 + (aux_X[:, 3] - aux_X[:, 1]) ** 2
+    ).double()
+
+    return torch.stack(
+        [intercept, -goal_distance, min_obs_dist], dim=1
+    ).double()
