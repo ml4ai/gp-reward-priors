efe2c679520fb3a75ed02d3868318db3d0b83f3d
Loren Rieffer-Champlin <champlin@arizona.edu>
2026-05-12 11:52:37 -0700

    trying to prevent explosion

--- optbnn/prior_mappers/wasserstein_gp_mapper.py
+++ optbnn/prior_mappers/wasserstein_gp_mapper.py
@@ -134,6 +134,7 @@ class MapperWassersteinGP(object):
                 prior_optimizer.zero_grad()
                 losses = torch.vmap(compute_sqw2)(X_batch, aux_X_batch)
                 loss = losses.sum() / X_batch.size(0)
+                assert torch.isfinite(loss).all()
                 loss.backward()
                 check_gradients(self.bnn)
                 prior_optimizer.step()
